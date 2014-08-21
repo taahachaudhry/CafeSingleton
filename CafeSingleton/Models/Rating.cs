b8bbca0e5c671124aa4fb198868b28f31aff35c0
taahachaudhry <t44h413@gmail.com>
2014-08-21 15:48:42 -0500

    Review class

---
+++ CafeSingleton/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CafeSingleton.Models
+{
+    public enum Rating
+    {
+        one = 1,
+        two,
+        three,
+        four,
+        five
+    }
+}
---
+++ CafeSingleton/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeSingleton.Models
+{
+    public class Review
+    {
+        protected static int NextID = 0;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
