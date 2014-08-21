4525802593b0ee0b7eda6f8a81b3c7bcd75e4155
taahachaudhry <t44h413@gmail.com>
2014-08-21 15:41:37 -0500

    Cafe class

---
+++ CafeSingleton/Models/Cafe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeSingleton.Models
+{
+    public class Cafe
+    {
+        protected static int NextID = 0;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public Cafe(int id, string name, string city)
+        {
+            ID = id;
+            Name = name;
+            City = city;
+        }
+        public Cafe()
+        {
+
+        }
+    }
+}
---
+++ CafeSingleton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CafeSingleton.Startup))]
+namespace CafeSingleton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
