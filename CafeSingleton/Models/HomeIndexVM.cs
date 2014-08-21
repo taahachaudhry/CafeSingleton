53de5b1d375aa981a7d21e757b341507810686cd
taahachaudhry <t44h413@gmail.com>
2014-08-21 15:49:51 -0500

    VM

---
+++ CafeSingleton/Models/HomeIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeSingleton.Models
+{
+    public class HomeIndexVM
+    {
+        public List<Cafe> Cafes { get; set; }
+    }
+}
